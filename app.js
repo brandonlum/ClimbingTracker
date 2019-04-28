9c17b74d172771b1f64bf7e3ee27f8949785d872
brandonlum <brandon.lum09@gmail.com>
2019-04-28 00:41:26 -0400

    Display current weather at location (currently Defaulted)

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ Course: SEI-Flex (2019)
 ////////////////////////////////
 
   // Weather 
-  const weatherBase = `api.openweathermap.org/data/2.5/weather?`;
+  const weatherBase = `http://api.openweathermap.org/data/2.5/weather?`;
   const id = `NWY1MjU4NmU2NWQyNjhiYzQwMjRhNzFkN2E4NjgzODc=`;
   const dcId = window.atob(id);
   const fullId = `APPID=${dcId}`;
@@ -31,14 +31,51 @@ Course: SEI-Flex (2019)
 
   // Weather
   const getWeather = () => {
-    console.log(fullWeatherPath) // Testing Weather path
+    console.log(fullWeatherPath); // Testing Weather path
+    $.ajax({
+      url: fullWeatherPath,
+      type: "Get"//,
+      // data: {
+      //   "$limit": 1;
+      // }
+    }).then((weatherData) => {
+      const $city = $('<h2>').text(inputLocation).css({'text-transform': 'capitalize'});
+      let currentTempFah = convKeltoFah(weatherData.main.temp);
+      let currentTempCel = convKeltoCel(weatherData.main.temp);
+      let tempMinFah = convKeltoFah(weatherData.main.temp_min);
+      let tempMaxFah = convKeltoFah(weatherData.main.temp_max);
+      let tempMinCel = convKeltoCel(weatherData.main.temp_min);
+      let tempMaxCel = convKeltoCel(weatherData.main.temp_max);
+      const $currentTempCont = $('<div>').text(`Current Temp = ${currentTempFah + String.fromCharCode(176)}F/${currentTempCel + String.fromCharCode(176)}C`);
+      const $tempMinMax = $('<div>').text(`Temp Range = ${tempMinFah + String.fromCharCode(176)}F/${tempMinCel + String.fromCharCode(176)}C - ${tempMaxFah + String.fromCharCode(176)}F/${tempMaxCel + String.fromCharCode(176)}C`);  
+      $('.content-container').append($city).append($currentTempCont).append($tempMinMax)
+    },
+    (error) => {
+      console.error(error)
+    })
   }
 
+  
+  
 
+////////////////////////////////
+// Functions
+////////////////////////////////
 
+  // Convert
+    // Kelvin to Fahrenheit
+    const convKeltoFah = (tempKel) => {
+      return ((tempKel-273.15)*(9/5)+32).toFixed(2); // convert from Kelvin to Fahrenheit
+    };
+    // Kelvin to Celcius
+    const convKeltoCel = (tempKel) => {
+      return (tempKel-273.15).toFixed(2); // convert from Kelvin to Celcius
+    };
 
  $(() => {
 
-   getWeather();
+   $('#nav-weather').on('click', (event) => {
+     getWeather();
+   })
    
  })
