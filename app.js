a1e60b291626847c5da4dd6349f13329efb6e217
brandonlum <brandon.lum09@gmail.com>
2019-04-27 23:17:17 -0400

    Initial API setup and nav bar built

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ Author: Brandon Lum
 Course: SEI-Flex (2019)
 */
 
+// console.log('Linked JS')
 
-console.log('Linked JS')
+
+////////////////////////////////
+// API Setup
+////////////////////////////////
+
+  // Weather 
+  const weatherBase = `api.openweathermap.org/data/2.5/weather?`;
+  const id = `NWY1MjU4NmU2NWQyNjhiYzQwMjRhNzFkN2E4NjgzODc=`;
+  const dcId = window.atob(id);
+  const fullId = `APPID=${dcId}`;
+  const queryStarter = `q=`;
+  let inputLocation = `new york`; // lowercase
+
+  let fullWeatherPath = weatherBase + queryStarter + inputLocation + `&` + fullId;
+
+
+
+
+
+////////////////////////////////
+// Querying API
+////////////////////////////////
+
+  // Weather
+  const getWeather = () => {
+    console.log(fullWeatherPath) // Testing Weather path
+  }
+
+
+
 
  $(() => {
+
+   getWeather();
    
  })
